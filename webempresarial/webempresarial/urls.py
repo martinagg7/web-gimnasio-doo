ed7f85fcd052c594e2d298b9be05271b9d825340
Martina Garcia <martinagg@myuax.com>
2024-11-29 18:17:15 +0100

    vercel-url

--- webempresarial/webempresarial/urls.py
+++ webempresarial/webempresarial/urls.py
@@ -30,6 +30,7 @@ urlpatterns = [
 
 ]
 
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 if settings.DEBUG:
     from django.conf.urls.static import static
     urlpatterns += static(settings.MEDIA_URL,
