788f69bc30ecd35783d22a3aa670d92d88876b17
Martina Garcia <martinagg@myuax.com>
2024-11-29 18:29:12 +0100

    +cambios

--- webempresarial/webempresarial/settings.py
+++ webempresarial/webempresarial/settings.py
@@ -49,22 +49,10 @@ INSTALLED_APPS = [
 ]
 #ckeditor
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 CKEDITOR_UPLOAD_PATH = "uploads/"
 
-CKEDITOR_CONFIGS = {
-    'default': {
-        'toolbar': 'Custom',
-        'toolbar_Custom': [
-            ['Bold', 'Italic', 'Underline'],
-            ['NumberedList', 'BulletedList', '-', 'Outdent', 'Indent',
-'-',
-            'JustifyLeft', 'JustifyCenter', 'JustifyRight',
-'JustifyBlock'],
-            ['Link', 'Unlink']
-        ]
-    }
-}
+
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
